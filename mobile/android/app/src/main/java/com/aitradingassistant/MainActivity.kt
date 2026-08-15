89f36bdc0c170ea516a112ef85b2b6d446be6bc4
Shanoor-Abdul <abdulshanoorcoder@gmail.com>
2026-08-15 15:58:12 +0300

    fix: resolve React Native UnsatisfiedLinkError by avoiding DefaultNewArchitectureEntryPoint and adding INTERNET permission

--- mobile/android/app/src/main/java/com/aitradingassistant/MainActivity.kt
+++ mobile/android/app/src/main/java/com/aitradingassistant/MainActivity.kt
@@ -3,14 +3,13 @@ package com.aitradingassistant
 import android.content.Intent
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "AITradingAssistantMobile"
 
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-    DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    DefaultReactActivityDelegate(this, mainComponentName, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED)
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
     super.onActivityResult(requestCode, resultCode, data)
